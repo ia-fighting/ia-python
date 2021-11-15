d901bb1f5d48bf7648ccaa906045754d70d9258a
Marion MOTA <katyanne_mm@live.fr>
2021-11-15 16:10:10 +0100

    feat: Rules

--- core/Environment.py
+++ core/Environment.py
@@ -1,4 +1,31 @@
 # Game environment class
+
+ARENA = """
+#.              *     #
+"""
+
+START = '.'
+GOAL = '*'
+WALL = '#'
+
+# Rewards
+REWARD_OUT = -25
+REWARD_TOUCH_TARGET = -25
+REWARD_EMPTY = -1
+REWARD_BLOCK = -5
+REWARD_WOUND_TARGET = 10
+REWARD_KILL_TARGET = 100
+REWARD_BEING_TOUCH = -20
+REWARD_TOUCH_EMPTY = -3
+
+# Possible actions
+RIGHT = 'R'
+LEFT = 'L'
+PUNCH = 'P'
+BLOCK = 'B'
+ACTIONS = [RIGHT, LEFT, PUNCH, BLOCK]
+
+
 class Environment:
     def __init__(self, name):
         self.name = name
