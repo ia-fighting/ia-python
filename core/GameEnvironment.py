c5fbf1a2dd0cc24d3fdfdd12599e7d4b097411bb
Marion MOTA <katyanne_mm@live.fr>
2021-11-15 20:55:32 +0100

    feat: Adding Agent

--- core/GameEnvironment.py
+++ core/GameEnvironment.py
@@ -72,7 +72,16 @@ class GameEnvironment:
 
     @property
     def goal(self):
-        return not self.target.is_alive
+        return not self.__target.is_alive
+
+    def is_near_target(self, state, states):
+        if state[1] < len(ARENA) - 1:
+            if states[state[0], state[1] + 1] == TARGET:
+                return True
+        if state[1] >= 1:
+            if states[state[0], state[1] - 1] == TARGET:
+                return True
+        return False
 
     # Appliquer une action sur l'environnement
     # On met à jour l'état de l'agent, on lui donne sa récompense
@@ -83,27 +92,103 @@ class GameEnvironment:
         if action == RIGHT:
             new_state = (state[0], state[1] + 1)
 
-        # should not be inf or sup to the arena size
-        right_state = (state[0], state[1] + 2)
-        left_state = (state[0], state[1] - 2)
         # Calcul recompense agent et lui transmettre
         if new_state in self.__states:
             if self.__states[new_state] in [WALL, TARGET]:
                 reward = REWARD_OUT
-            elif self.__states[right_state] == TARGET and action == PUNCH:
-                reward = REWARD_WOUND_TARGET
-                self.target.health -= 20
-            elif self.__states[left_state] == TARGET and action == PUNCH:
+            elif action == PUNCH and self.is_near_target(state, self.__states):
                 reward = REWARD_WOUND_TARGET
                 self.target.health -= 20
             elif action == BLOCK:
                 reward = REWARD_BLOCK
             else:
                 reward = REWARD_EMPTY
-            if not self.target.is_alive:
+            if not self.__target.is_alive:
                 reward = REWARD_KILL_TARGET
             state = new_state
         else:
             reward = REWARD_OUT
         agent.update(action, state, reward)
         return reward
+
+class Agent:
+    def __init__(self, environment):
+        self.__state = environment.start
+        self.__score = 0
+        self.__last_action = None
+        self.__qtable = {}
+
+        # QTable initialization
+        for s in environment.states:
+            self.__qtable[s] = {}
+            for a in ACTIONS:
+                self.__qtable[s][a] = 0.0
+
+    def update(self, action, new_state, reward):
+        # QTable update
+        # Q(s, a) <- Q(s, a) + learning_rate * [reward + discount_factor * max(qtable[a]) - Q(s, a)]
+        maxQ = max(self.__qtable[new_state].values())
+        LEARNING_RATE = 1
+        DISCOUNT_FACTOR = 0.5
+
+        self.__qtable[self.__state][action] += LEARNING_RATE * \
+                        (reward + DISCOUNT_FACTOR * maxQ - self.__qtable[self.__state][action])
+
+        self.__state = new_state
+        self.__score += reward
+        self.__last_action = action
+
+    # Best action who maximise reward
+    def best_action(self):
+        possible_rewards = self.__qtable[self.__state]
+        best = None
+        for a in possible_rewards:
+            if best is None or possible_rewards[a] > possible_rewards[best]:
+                best = a
+        return best
+
+    @property
+    def state(self):
+        return self.__state
+
+    @property
+    def score(self):
+        return self.__score
+
+    @property
+    def qtable(self):
+        return self.__qtable
+
+    def reset(self, environment):
+        self.__state = environment.start
+
+
+if __name__ == '__main__':
+    target = Target(60)
+    env = GameEnvironment(target, ARENA)
+    print(env.states)
+
+    agent = Agent(env)
+
+    # for i in range(3):
+    iteration = 0
+    while agent.state != env.goal:
+        iteration += 1
+        action = agent.best_action()
+        # print(action)
+        reward = env.apply(agent, action)
+        print(iteration, agent.state, agent.score, reward)
+        # print(agent.qtable)
+
+    agent.reset(env)
+
+    print()
+
+    iteration = 0
+    while agent.state != env.goal:
+        iteration += 1
+        action = agent.best_action()
+        print(action)
+        reward = env.apply(agent, action)
+        print(iteration, agent.state, agent.score, reward)
+        # print(agent.qtable)
